da86302ae9112ae130b949aa9f552b8a81d7a258
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2024-04-13 16:28:58 +0200

    Small backend changes, !GRAPH TO BE DONE!

--- backend/src/main/java/com/LogisticsCalculator/LogisticsCalculator/Controllers/MiddlemanController.java
+++ backend/src/main/java/com/LogisticsCalculator/LogisticsCalculator/Controllers/MiddlemanController.java
@@ -1,19 +1,23 @@
 package com.LogisticsCalculator.LogisticsCalculator.Controllers;
 
+import com.LogisticsCalculator.LogisticsCalculator.Services.CPM.CPMService;
+import com.LogisticsCalculator.LogisticsCalculator.Services.Middleman.MiddlemanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/middleman")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MiddlemanController {
-    //private final TaskRepository taskRepository;
-
+    private final MiddlemanService _middlemanService;
     @Autowired
-    public MiddlemanController() {
-
+    public MiddlemanController(MiddlemanService middlemanService) {
+        this._middlemanService = middlemanService;
     }
+
     @GetMapping()
     public int callMiddleman(){
         return 2;
--- backend/src/main/java/com/LogisticsCalculator/LogisticsCalculator/Services/Middleman/MiddlemanService.java
+++ backend/src/main/java/com/LogisticsCalculator/LogisticsCalculator/Services/Middleman/MiddlemanService.java
@@ -1,4 +1,8 @@
 package com.LogisticsCalculator.LogisticsCalculator.Services.Middleman;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MiddlemanService {
+
 }
